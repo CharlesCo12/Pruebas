49d4fbfe8fc123f006df72dbca5b452c5dc980c8
CharlesCo12 <49194752+CharlesCo12@users.noreply.github.com>
2019-05-16 22:37:38 -0500

    Add files via upload

---
+++ bifurcation_plot.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+def modulo(A):
+    n=len(A)
+    A1=np.zeros(n)
+    for i in range(n):
+        x=A[i]
+        if(x>np.pi):
+            while(x>np.pi):
+                x=x-np.pi
+        if(x<np.pi):
+            while(x<-np.pi):
+                x=x+np.pi
+        A1[i]=x
+    return A1
+def logistic(r, x):
+    return r * x * (1 - x)
+n = 10000
+r = np.linspace(2.8, 4.0, n)
+iterations = 1000
+last = 100
+x = np.abs(np.min(modulo(np.loadtxt("datos_bif.dat"))))*np.ones(10000)
+for i in range(iterations):
+    x = logistic(r, x)
+    # We display the bifurcation diagram.
+    if i >= (iterations - last):
+        plt.plot(r, x, ',k', alpha=.06)
+plt.xlim(2.8, 4)
+plt.title("Bifurcation diagram")
+plt.savefig('bif.png')
